dffa940388c1c41ce8bdfb1d6b5a11fa3dc4df6f
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-15 14:17:13 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+ let multiply=num1*num2;
+
+ let random=4;
+ Math.random()*10;
+
+ 
+ let num3=10;
+ let num4=6;
+ let mod=num3-num4;
+
+ let max=Math.max(17,20,9,7,8,19,13)
